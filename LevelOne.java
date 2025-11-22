57e3533efac6fdd89767a86706c8205be7f296f6
Zay <156462710+Gowwy@users.noreply.github.com>
2025-11-22 13:31:40 -0500

    Progress Bar
    Progress bar.
    Once you click start game youll enter "level 1" and the bar will be at the bottom center. Pressing W will increase value and L will decrease

---
+++ LevelOne.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LevelOne here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LevelOne extends World
+{
+    public ProgressBar progress;
+
+    public LevelOne()
+    {
+        super(600, 400, 1);
+
+        progress = new ProgressBar(400, 20, 100);
+        addObject(progress, getWidth()/2, getHeight() - 20);
+    }
+}
--- StartButton.java
+++ StartButton.java
@@ -18,8 +18,8 @@ public class StartButton extends Actor
     {
         image.scale(200,50);
         setImage(image);
-         if (Greenfoot.mouseClicked(this)) {
-            System.out.println("Button clicked!");
+        if (Greenfoot.mouseClicked(this)) {
+            Greenfoot.setWorld(new LevelOne());
         }
     }
 }
